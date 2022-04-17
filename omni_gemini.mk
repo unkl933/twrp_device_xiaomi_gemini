f5d64f85cace85285e168c5c4e2c9a4f108f931a
dopamine <crack1933@gmail.com>
2022-04-18 00:02:49 +0700

    Update omni_gemini.mk

--- omni_gemini.mk
+++ omni_gemini.mk
@@ -20,7 +20,7 @@ $(call inherit-product, build/target/product/embedded.mk)
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 # $(call inherit-product, vendor/omni/config/gsm.mk)
 # Time Zone data for recovery
 
