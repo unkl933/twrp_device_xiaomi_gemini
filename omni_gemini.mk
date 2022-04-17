055e24e3aa545b5cf47d7479026e2fbcd95a39ab
dopamine <crack1933@gmail.com>
2022-04-17 18:59:47 +0700

    test

--- omni_gemini.mk
+++ omni_gemini.mk
@@ -16,15 +16,29 @@
 
 # Release name
 PRODUCT_RELEASE_NAME := gemini
-
 $(call inherit-product, build/target/product/embedded.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+# $(call inherit-product, vendor/omni/config/gsm.mk)
+# Time Zone data for recovery
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gemini
 PRODUCT_NAME := omni_gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MANUFACTURER := xiaomi
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.allow.mock.location=0 \
+    ro.hardware.keystore=msm9886
+
+# Blacklist Properties
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.build.date \
+    ro.build.date.utc
