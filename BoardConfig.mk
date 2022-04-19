8bda13970f419fb0b834591b714e3ec1b1fcf1ff
dopamine <crack1933@gmail.com>
2022-04-20 02:45:13 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,13 @@
 #
-# Copyright 2016 The Android Open Source Project
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,14 +16,7 @@
 # limitations under the License.
 #
 
-# This contains the module build definitions for the hardware-specific
-# components for this device.
-#
-# As much as possible, those components should be built unconditionally,
-# with device-specific names to avoid collisions, to avoid device-specific
-# bitrot and build breakages. Building a component unconditionally does
-# *not* include it on all devices, so it is safe even with hardware-specific
-# components.
+DEVICE_PATH := device/xiaomi/gemini
 
 # Architecture
 TARGET_ARCH := arm64
@@ -29,7 +24,6 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := kryo
-TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
@@ -37,30 +31,15 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8996
 TARGET_NO_BOOTLOADER := true
 
-# Kernel
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff loop.max_part=7
-BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1 androidboot.configfs=true
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x01000000
-TARGET_PREBUILT_KERNEL := device/xiaomi/gemini/prebuilt/Image.gz-dtb
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8996
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno530
-
-# Encryption
-# TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
-# TARGET_HW_DISK_ENCRYPTION := true
-TW_INCLUDE_CRYPTO := true
+# Assert
+TARGET_OTA_ASSERT_DEVICE := gemini
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
@@ -69,54 +48,81 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 58846064640
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 
-# Recovery
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+# File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+TARGET_PREBUILT_KERNEL := device/xiaomi/gemini/prebuilt/Image.gz-dtb
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/xiaomi/gemini
+TARGET_KERNEL_CONFIG := gemini_defconfig
 
 # Ramdisk compression
 LZMA_RAMDISK_TARGETS := recovery,boot
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
-TARGET_RECOVERY_FSTAB := device/xiaomi/gemini/recovery.fstab
+# Platform
+TARGET_BOARD_PLATFORM := msm8996
 
-TARGET_OTA_ASSERT_DEVICE := gemini
+TW_INCLUDE_FBE := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+TW_INCLUDE_CRYPTO := true
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
 
+# TWRP Configuration
+TW_INPUT_BLACKLIST := "hbtp_vm"
+# TW_USE_TOOLBOX := true
+TW_INCLUDE_FB2PNG := true
 # TWRP specific build flags
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 2047
-TW_MAX_BRIGHTNESS := 4095
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
-TW_DEFAULT_LANGUAGE := en
-TW_HAS_EDL_MODE := true
-TW_SKIP_COMPATIBILITY_CHECK := true
-TW_INCLUDE_NTFS_3G := true
+TW_MAX_BRIGHTNESS := 4095
 TW_NO_USB_STORAGE := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
+TW_EXCLUDE_TWRPAPP := true
+TW_HAS_EDL_MODE := true
+TW_SKIP_COMPATIBILITY_CHECK := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+BOARD_SUPPRESS_SECURE_ERASE := true
 TW_IGNORE_MISC_WIPE_DATA := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_FUSE_EXFAT := true
+BOARD_VNDK_RUNTIME_DISABLE := true
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
 
-# System-as-root
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
 
 # Treble
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
 TARGET_COPY_OUT_VENDOR := vendor
-
+#
 # Logcat
-# TARGET_USES_LOGD := true
-# TWRP_INCLUDE_LOGCAT := true
-
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
 TW_FORCE_USE_BUSYBOX := true
 TW_INCLUDE_RESETPROP := true
-
-TW_INPUT_BLACKLIST := "hbtp_vm"
-ALLOW_MISSING_DEPENDENCIES := true
